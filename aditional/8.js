193061fde0cbc51b07f3b63d6741281b7964b487
Raghav20112001 <raghavsethi4200@gmail.com>
2024-07-17 03:00:47 +0530

    Delete aditional directory

--- aditional/8.js
+++
@@ -1,7 +0,0 @@
-// Explain how you can remove the first object from an array and return that object ushing shift method?
-// TO REMOVE
-const myArray =[0, 1, 2, 3, 4, 5, 6]
-console.log(myArray[0]);
-myArray.shift()
-console.log(myArray);
-
